da0f1bc1d028ce18729b5e0c87a5c0f38ad75278
sonseongmin <sungmin2959@gmail.com>
2025-10-13 07:43:51 +0900

    f

--- lib/util/file_adapter.dart
+++ lib/util/file_adapter.dart
@@ -1,16 +1,23 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'stub_adapter.dart'
-    if (dart.library.html) 'web_file_adapter.dart'
-    if (dart.library.io) 'mobile_file_adapter.dart';
 import 'file_adapter_stub.dart';
+
+// 조건부 import: Web / Mobile / Stub
+import 'web_file_adapter.dart'
+  if (dart.library.io) 'mobile_file_adapter.dart'
+  if (dart.library.html) 'web_file_adapter.dart'
+  if (dart.library.js) 'web_file_adapter.dart'
+  if (dart.library.io) 'mobile_file_adapter.dart'
+  if (dart.library.io) 'mobile_file_adapter.dart'
+  if (dart.library.html) 'web_file_adapter.dart';
+import 'stub_adapter.dart' if (dart.library.io) 'mobile_file_adapter.dart';
+
+final IFileAdapter _impl = createFileAdapter();
+
 class FileAdapter {
-  final IFileAdapter _impl = createFileAdapter();
+  static Future<void> pickAndUpload(BuildContext context, String exercise) =>
+      _impl.pickAndUpload(context, exercise);
 
-  Future<void> pickAndUpload(BuildContext context, String exercise) async {
-    await _impl.pickAndUpload(context, exercise);
-  }
-
-  Future<void> openCamera(BuildContext context, String exercise) async {
-    await _impl.openCamera(context, exercise);
-  }
+  static Future<void> openCamera(BuildContext context, String exercise) =>
+      _impl.openCamera(context, exercise);
 }
