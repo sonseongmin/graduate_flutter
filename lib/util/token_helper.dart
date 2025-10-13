39f41be5bfaca78892b9e14e19856141d4848ae0
sonseongmin <sungmin2959@gmail.com>
2025-10-14 01:47:43 +0900

    token

--- lib/util/token_helper.dart
+++ lib/util/token_helper.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/foundation.dart';
+import 'dart:html' as html; // 웹 토큰용
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
 class TokenHelper {
@@ -6,11 +7,15 @@ class TokenHelper {
 
   static Future<String?> getToken() async {
     try {
-      return await _storage.read(key: 'access_token');
-    } catch (e) {
-      if (kDebugMode) {
-        print('토큰 가져오기 오류: $e');
+      if (kIsWeb) {
+        // ✅ 웹에서는 localStorage 사용
+        return html.window.localStorage['access_token'];
+      } else {
+        // ✅ 모바일에서는 secure storage 사용
+        return await _storage.read(key: 'access_token');
       }
+    } catch (e) {
+      if (kDebugMode) print('토큰 가져오기 오류: $e');
       return null;
     }
   }
